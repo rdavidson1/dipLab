ba08f684409d7b6bbbb63fb979b5e2e8e6f4825b
rdavidson1 <rdavidson1@L11713042776.wctc.msft>
2014-09-18 15:28:17 -0500

    Created interface and started editing subclasses

--- src/dip/lab2/FoodServiceTipCalculator.java
+++ src/dip/lab2/FoodServiceTipCalculator.java
@@ -9,12 +9,12 @@ package dip.lab2;
  * @author your name goes here
  */
 public class FoodServiceTipCalculator {
-    private static final double MIN_BILL = 0.00;
-    private static final String BILL_ENTRY_ERR =
-            "Error: bill must be greater than or equal to " + MIN_BILL;
-    private static final double GOOD_RATE = 0.20;
-    private static final double FAIR_RATE = 0.15;
-    private static final double POOR_RATE = 0.10;
+    private double minBill = 0.00;
+    private String billEntryErr =
+            "Error: bill must be greater than or equal to " + minBill;
+    private double goodRate = 0.20;
+    private double fairRate = 0.15;
+    private double poorRate = 0.10;
 
     private double bill;
     public enum ServiceQuality {
